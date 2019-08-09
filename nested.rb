22d959a649a0a4d55788024b34dad353eec557ed
Tianlin Zheng <zhengt@mit.edu>
2019-08-09 16:13:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  programmer_hash[:alan_kay][:known_for]
+  programmer_hash[:alan_kay][:languages]
 end
 
 def adding_matz
